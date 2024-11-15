c6f7cc9d012898183771a5c347c82c02f9b26045
Sorahon84 <shohkuch@gmail.com>
2024-11-15 12:02:18 -0600

    second commit

--- main/java/gitLearn/Test.java
+++ main/java/gitLearn/Test.java
@@ -3,6 +3,7 @@ package gitLearn;
 public class Test {
     public static void main(String[] args) {
         System.out.println("this is the project version 1");
-      
+        System.out.println("this is the project version 2");
+
     }
 }
